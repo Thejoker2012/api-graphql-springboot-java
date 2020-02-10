dca171db425682e158db96dc5c2733a70e9c54f6
Iago Oliveira Santos <iagofalcom@hotmail.com>
2020-02-10 16:16:45 -0400

    Resolver Produto
    Interceptando o valor do produto e
    Formatando valor para reais

---
+++ src/main/java/com/udemy/compras/resolver/ProdutoResolver.java
@@ -0,0 +1,13 @@
+package com.udemy.compras.resolver;
+
+import com.coxautodev.graphql.tools.GraphQLResolver;
+import com.udemy.compras.models.Produto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProdutoResolver implements GraphQLResolver<Produto> {
+
+    public String valorReais(Produto p){
+        return "R$"+p.getValor();
+    }
+}
