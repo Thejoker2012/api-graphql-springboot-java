f299575232d3ce26879ddc03bfb6cab5cff6d0e3
Iago Oliveira Santos <iagofalcom@hotmail.com>
2020-02-10 18:14:42 -0400

    Query Customizada

--- src/main/java/com/udemy/compras/repository/CompraRepository.java
+++ src/main/java/com/udemy/compras/repository/CompraRepository.java
@@ -3,6 +3,7 @@ package com.udemy.compras.repository;
 import com.udemy.compras.models.Cliente;
 import com.udemy.compras.models.Compra;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +11,8 @@ import java.util.List;
 @Repository
 public interface CompraRepository extends JpaRepository<Compra, Long> {
 
+    //Query customizada
+   // @Query("select c from Compra where c.cliente = :cliente");
     List<Compra> findAllByCliente(Cliente c);
 
 }
