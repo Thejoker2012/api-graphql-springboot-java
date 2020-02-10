4652d38d2d89aaa88f669df698447285dad0c8fd
Iago Oliveira Santos <iagofalcom@hotmail.com>
2020-02-10 12:52:30 -0400

    Implementando Mutation de save e delete Cliente

--- src/main/java/com/udemy/compras/graphql/ClienteGraphQL.java
+++ src/main/java/com/udemy/compras/graphql/ClienteGraphQL.java
@@ -1,25 +1,50 @@
 package com.udemy.compras.graphql;
 
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import com.udemy.compras.Cliente;
 import com.udemy.compras.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Component
-public class ClienteGraphQL implements GraphQLQueryResolver {
+public class ClienteGraphQL implements GraphQLQueryResolver, GraphQLMutationResolver {
 
     @Autowired
     private ClienteRepository clienteRepository;
 
+    //Buscar um Cliente passando o id
     public Cliente cliente(Long id){
         return clienteRepository.findById(id).orElse(null);//Sempre que houver retorno Optional usar orElse
     }
-
+    //Buscar Todos os Clientes
     public List<Cliente> clientes(){
         return clienteRepository.findAll();
     }
 
+    //Salvar
+    //Atualizar o Cliente passando o Id
+    @Transactional
+    public Cliente saveCliente(Long id, String nome, String email){
+        Cliente c = new Cliente();
+        c.setId(id);
+        c.setNome(nome);
+        c.setEmail(email);
+        return clienteRepository.save(c);
+    }
+
+    //Deletar Cliente passando id
+    //Faz a busca com o findByid se houver o cliente ele exclui e retorna true
+    //caso não exista retorna false
+    @Transactional
+    public Boolean deleteCliente(Long id){
+        if (clienteRepository.findById(id).isPresent()){
+            clienteRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
 }
